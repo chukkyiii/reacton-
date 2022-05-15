49dbc2d45141b206500371d8256f6c1e9f61db49
Jesse <c1022253@ncl.ac.uk>
2022-05-15 07:57:23 +0100

    more styles for reactbox

--- frontend/src/components/Reactbox.jsx
+++ frontend/src/components/Reactbox.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import styles from './Reactbox.module.css'
 
 export default function Reactbox() {
-
+    // setup the state
+    
     return (
         <div>
             <div className={styles.test_container}>
-                <button> {/* react box */}</button>
-                <div>{/* test bars */}</div>
+                <button className={styles.reac_container}> {/* react box */}</button>
+                <div className={styles.test_bar}>
+                    <div className={styles.bar_item}>
+                        {/* previous timer */}
+                    </div>
+                    <div className={styles.bar_item}>
+                        {/* current timer */}
+                    </div>
+                    <button className={styles.reset_button}>
+                        {/* reset button and start button */}
+                    </button>
+                </div>
             </div>
         </div>
     )
