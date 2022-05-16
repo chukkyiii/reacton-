9ab62821e612b499a4aaf3cca476f0e68d204387
Jesse <c1022253@ncl.ac.uk>
2022-05-16 18:31:50 +0100

    navigation component easy to add new link

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {NavLink, useLocation } from 'react-router-dom'
+import styles from './Navigation.module.css'
+
+export default function Navigation() {
+    const menu = [
+        {title: 'Home', path: '/'},
+    ]
+
+    const location = useLocation()
+    return (
+        <div>
+            <div className={styles.container}>
+                <div className={styles.loading_bar}></div>
+                <h1 className={styles.title_stlye}>Reacton<span className={styles.highlight}>.tech</span> </h1>
+                <header className={styles.nav_container}>
+                    <nav className={styles.nav_block}>
+                        <ul className={styles.nav_links}>
+                            {menu.map((item, index) => {
+                                return (
+                                <li key={index}>
+                                    <NavLink to={item.path}
+                                            className = {
+                                                location.pathname === item.path 
+                                                ? styles.active
+                                                : styles.non_active
+                                            }>
+                                        {item.title}
+                                    </NavLink>
+                                </li>
+                                )
+                            })}
+                        </ul>
+                    </nav>
+                </header>
+            </div>
+        </div>
+    )
+}
