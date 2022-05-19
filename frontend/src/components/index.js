1ad69b123dbf424b53ee5cce66ab3eb39a8b00b0
Jesse <c1022253@ncl.ac.uk>
2022-05-19 21:20:23 +0100

    added new exports

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,7 @@
 export { default as Reactbox } from "./Reactbox";
 export { default as Navigation } from "./Navigation";
+export { default as About } from "./About";  
+export { default as Login } from "./Login"; 
+export { default as Logout } from "./Logout";
+export { default as Profile } from "./Profile";
+export { default as useToken } from "./useToken";
